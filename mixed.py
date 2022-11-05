90de41caff8a7fcc8fc8461d49a6e8fcd6fdbe38
Vurghuna Baghirova <117365597+Vurghuna@users.noreply.github.com>
2022-11-05 04:24:06 +0400

    Update and rename 04.py to mixed.py

--- mixed.py
+++ mixed.py
@@ -1,5 +1,3 @@
-Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> num1=3
 >>> num2=5
 >>> type(num1)
@@ -14,10 +12,7 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> num1=10284,2
 >>> num2=2
 >>> result=num1/num2
-Traceback (most recent call last):
-  File "<pyshell#9>", line 1, in <module>
-    result=num1/num2
-TypeError: unsupported operand type(s) for /: 'tuple' and 'int'
+result=num1/num2
 >>> num1=10234.23
 >>> num2=3231.1
 >>> result=num1/num2
